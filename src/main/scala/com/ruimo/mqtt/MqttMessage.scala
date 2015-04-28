1ed8659edcf2a2ec30093981b9ed517aa02daeb2
Shisei Hanai <ruimo.uno@gmail.com>
2015-04-28 11:20:05 +0900

    Use immutable byte array.

--- src/main/scala/com/ruimo/mqtt/MqttMessage.scala
+++ src/main/scala/com/ruimo/mqtt/MqttMessage.scala
@@ -1,19 +1,19 @@
 package com.ruimo.mqtt
 
 import org.eclipse.paho.client.mqttv3.{MqttMessage => PahoMqttMessage}
+import com.ruimo.scoins.ImmutableByteArray
 
 // Immutable MqttMessage.
 case class MqttMessage(
   qos: Qos,
   isRetained: Boolean,
-  payload: Seq[Byte]
+  payload: ImmutableByteArray
 )
 
 object MqttMessage {
   def apply(m: PahoMqttMessage): MqttMessage = MqttMessage(
     qos = Qos(m.getQos),
     isRetained = m.isRetained,
-    payload = m.getPayload.clone
+    payload = ImmutableByteArray(m.getPayload)
   )
 }
-

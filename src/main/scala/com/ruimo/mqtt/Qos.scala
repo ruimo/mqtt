d7118ebd98072c4fd8d3a7135d48a7cc59d5afdd
Shisei Hanai <ruimo.uno@gmail.com>
2015-04-28 10:51:40 +0900

    Move implicit conversion.

--- src/main/scala/com/ruimo/mqtt/Qos.scala
+++ src/main/scala/com/ruimo/mqtt/Qos.scala
@@ -3,10 +3,10 @@ package com.ruimo.mqtt
 import scala.language.implicitConversions
 
 abstract sealed class Qos(val code: Int) {
-  implicit def qosToInt(qos: Qos) = qos.code
 }
 
 object Qos {
+  implicit def qosToInt(qos: Qos) = qos.code
   case object AtMostOnce extends Qos(0)
   case object AtLeastOnce extends Qos(1)
   case object ExactlyOnce extends Qos(2)
